0f4f7cf3f8385fd1292004ca1388b7fa797230b0
AlyusPhoenix <72719965+AlyusPhoenix@users.noreply.github.com>
2023-01-29 00:16:47 +0800

    Setup

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Engine } from "babylonjs";
+import { App } from "./app";
+//console.log('hello xr');
+
+const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+// const ctxt = canvas.getContext('2d');
+// ctxt.font = '50px Arial';
+// ctxt.fillText('Hello XR', 50, 50);
+
+// creates new engine
+const engine = new Engine(canvas, true);
+
+const app = new App(engine, canvas);
+const scenePromise = app.createXRScene();
+// engine.runRenderLoop(() => {
+//     scene.render();
+// })
+
+// Async VR scene
+scenePromise.then(scene => {
+    engine.runRenderLoop(() => {
+        scene.render();
+    })
+})
