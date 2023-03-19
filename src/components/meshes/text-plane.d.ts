22e03bfaa0cd91fd9c53aaf9cddeebc717de671f
AlyusPhoenix <72719965+AlyusPhoenix@users.noreply.github.com>
2023-03-19 22:15:38 +0800

    commit

--- src/components/meshes/text-plane.d.ts
+++
@@ -1,6 +0,0 @@
-import { Scene } from 'babylonjs';
-import { TextBlock } from 'babylonjs-gui';
-export declare class TextPlane {
-    textBlock: TextBlock;
-    constructor(name: string, width: number, height: number, x: number, y: number, z: number, text: string, backgroundColor: string, textColor: string, fontSize: number, scene: Scene);
-}
